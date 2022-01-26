5b9bbab2b4a34f87fc66f8ca22af765f8dcdc418
Yimning <1148967988@qq.com>
2022-01-26 15:50:52 +0800

    Update Protocal

--- protocol/rtuacq_protocol_zonglin_NJXinRuiPeng_New.c
+++ protocol/rtuacq_protocol_zonglin_NJXinRuiPeng_New.c
@@ -312,8 +312,8 @@ int protocol_ZONGLIN_NJXinRuiPeng_New_info(struct acquisition_data *acq_data)
 
 		valf=getFloatValue(p, 19, dataType);
 		acqdata_set_value_flag(acq_data,"i13130",UNIT_MG_L,valf,INFOR_ARGUMENTS,&arg_n);
-
-		status=0;
+ 
+		status=0;  
 	}
 	
 	read_system_time(acq_data->acq_tm);
